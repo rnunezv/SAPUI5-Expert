fd7e607e5d396828f8716bcad18b06ffc4ff7fd7
rnunezv <rnunezv@gmailcom>
2021-03-01 17:45:00 +0000

    07_Interfaz_Usuario_Objetos-SAPUI5

--- Invoices/SAPUI5/webapp/Component.js
+++ Invoices/SAPUI5/webapp/Component.js
@@ -2,7 +2,8 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "logaligroup/SAPUI5/model/Models",
-    "sap/ui/model/resource/ResourceModel"
+    "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog"
 ],
 
     /**
@@ -10,7 +11,7 @@ sap.ui.define([
      * @param {typeof sap.ui.core.UIComponent} ResourceModel
      */
 
-    function (UIComponent, Models, ResourceModel) {
+    function (UIComponent, Models, ResourceModel, HelloDialog) {
 
         return UIComponent.extend("logaligroup.SAPUI5.Component", {
 
@@ -30,6 +31,18 @@ sap.ui.define([
                 var i18nModel = new ResourceModel({ bundleName: "logaligroup.SAPUI5.i18n.i18n" });
                 this.setModel(i18nModel, "i18n");
 
+                this._helloDialog = new HelloDialog(this.getRootControl());
+
+            },
+
+            exit: function () {
+                this._helloDialog.destroy();
+                delete this._helloDialog;
+            },
+
+            openHelloDialog: function (){
+                this._helloDialog.open();
             }
+
         });
     });
---
+++ Invoices/SAPUI5/webapp/controller/HelloDialog.js
@@ -0,0 +1,57 @@
+// @ts-nocheck
+sap.ui.define([
+    "sap/ui/base/ManagedObject",
+    "sap/ui/core/Fragment"
+],
+    /** 
+    * 
+    * @param {typeof sap.ui.base.ManagedObject} ManagedObject
+    * @param {typeof sap.ui.core.Fragment} Fragment
+    */
+    function (ManagedObject, Fragment) {
+        "use strict"
+
+        return ManagedObject.extend("logaligroup.SAPUI5.controller.HelloDialog", {
+
+            constructor: function (oView) {
+                this._oView = oView;
+            },
+
+            exit: function () {
+                delete this._oView;
+            },
+
+            open: function () {
+
+                const oView = this._oView;
+
+                // Create Dialog Lazily
+                if (!oView.byId("helloDialog")) {
+
+                    let oFragmentController = {                        
+                        onCloseDialog: function () {
+                            oView.byId("helloDialog").close();
+                        }
+                    };
+
+                    // Load asyncronous XLM fragment
+                    Fragment.load({
+
+                        id: oView.getId(),
+                        name: "logaligroup.SAPUI5.view.HelloDialog",
+                        controller: oFragmentController
+
+                    }).then(function (oDialog) {
+                        oView.addDependent(oDialog);
+                        oDialog.open();
+                    });
+
+                }
+                else {
+                    oView.byId("helloDialog").open();
+                }
+            }
+
+        });
+
+    });
